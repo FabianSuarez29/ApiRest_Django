4d4607c6e98faabda0b38246ac8072b41793bd18
Fabián Suarez <fesuareze@itc.edu.co>
2025-02-23 19:59:45 -0500

    Added "users" url at the Api Root

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,10 @@
 from rest_framework import routers
 from .api import ProjectViewSet
+from users.api import UserViewSet
+
 router = routers.DefaultRouter()
 
 router.register('api/projects', ProjectViewSet, 'projects')
+router.register('api/users', UserViewSet, 'users')
 
 urlpatterns = router.urls
