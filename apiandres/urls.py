6bd01ac2bb7eb180a83fb013b8720d8de46bc3f1
Fabián Suarez <fesuareze@itc.edu.co>
2025-03-03 13:49:42 -0500

    Unnecessary class

--- apiandres/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import routers
-#from .api import ApiAndresViewSet
-
-router = routers.DefaultRouter()
-
-#router.register('api/apiandres', ApiAndresViewSet, 'apiandres')
-
-urlpatterns = router.urls
